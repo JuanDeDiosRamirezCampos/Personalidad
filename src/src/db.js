1300f7e43d6f1022414165d3c82367384918d6d6
JuanDeDiosRamirezCampos <99991865+JuanDeDiosRamirezCampos@users.noreply.github.com>
2024-07-25 21:18:15 -0600

    Delete src directory

--- src/src/db.js
+++
@@ -1,9 +0,0 @@
-import { createPool } from "mysql2/promise";
-
-export const pool = createPool({
-    host: 'sql3.freesqldatabase.com',
-    user: 'sql3722180',
-    password: 'ppvWY22daI',
-    port: 3306,
-    database: 'sql3722180'
-})
