d6cc3a652afa468b455f139b5d3299fe02cbea88
JuanDeDiosRamirezCampos <juanmatacuicos@gmail.com>
2024-07-25 20:47:37 -0600

    first commit

--- src/controllers/formulario.controller.js
+++ src/controllers/formulario.controller.js
@@ -34,3 +34,13 @@ export const saveDatos = async (req, res) => {
   }
 };
 
+export const getData = async (req,res) => {
+    try{
+        const result = await pool.query('SELECT * FROM respuestas');
+        res.json(result[0]);
+
+    }catch (error){
+        console.error('Error al obtener los datos:', error);
+        res.status(500).json({ error: 'Error al obtener los datos' });
+    }
+}
